5a2d16a7a0773601bf52ee44d1b301b8bd19322b
Peter Kappelt <kappelt.peter@gmail.com>
2021-12-09 14:18:49 +0100

    Optional title for loading card

--- frontend/src/components/LoadingCard.tsx
+++ frontend/src/components/LoadingCard.tsx
@@ -1,16 +1,22 @@
-import { Card, Spin } from "antd";
+import { Card, Spin, Typography } from "antd";
 
-const LoadingCard = () => {
+const LoadingCard = ({ title }: { title?: string }) => {
   return (
     <Card style={{ marginTop: 16, marginBottom: 16 }}>
       <div
         style={{
           width: "100%",
           display: "flex",
+          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
         }}
       >
+        {title ? (
+          <div>
+            <Typography.Title>{title}</Typography.Title>
+          </div>
+        ) : null}
         <Spin size="large" />
       </div>
     </Card>
