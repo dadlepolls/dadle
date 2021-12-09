bf48bf8d39483eee99b0f5c8e62895d669b42758
Peter Kappelt <kappelt.peter@gmail.com>
2021-12-09 11:59:16 +0100

    graphql functions for fetching authenticated user and updating own name

--- backend/src/graphql/BuildSchema.ts
+++ backend/src/graphql/BuildSchema.ts
@@ -1,10 +1,11 @@
 import { graphAuthChecker } from "../auth/graphAuthChecker";
 import { buildSchema } from "type-graphql";
 import { PollResolver } from "./Poll";
+import { UserResolver } from "./User";
 
 const buildAppSchema = async () =>
   await buildSchema({
-    resolvers: [PollResolver],
+    resolvers: [PollResolver, UserResolver],
     authChecker: graphAuthChecker,
   });
 
---
+++ backend/src/graphql/User.ts
@@ -0,0 +1,88 @@
+import { ApolloError } from "apollo-server-errors";
+import { IGraphContext, IUser } from "src/util/types";
+import {
+  Arg,
+  Authorized,
+  Ctx,
+  Field,
+  ID,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import { User as UserModel } from "../db/models";
+
+@ObjectType()
+class User implements IUser {
+  @Field(() => ID)
+  _id: string;
+
+  @Field()
+  provider: "microsoft";
+
+  @Field()
+  idAtProvider: string;
+
+  @Field()
+  nameAtProvider: string;
+
+  @Field()
+  mail: string;
+
+  @Field()
+  name: string;
+
+  constructor(
+    _id: string,
+    provider: "microsoft",
+    idAtProvider: string,
+    nameAtProvider: string,
+    mail: string,
+    name: string
+  ) {
+    this._id = _id;
+    this.provider = provider;
+    this.idAtProvider = idAtProvider;
+    this.nameAtProvider = nameAtProvider;
+    this.mail = mail;
+    this.name = name;
+  }
+}
+
+@Resolver(User)
+class UserResolver {
+  @Authorized()
+  @Query(() => User)
+  async me(@Ctx() ctx: IGraphContext) {
+    if (!ctx.user?._id)
+      throw new ApolloError(
+        "User not authenticated!",
+        "USER_NOT_AUTHENTICATED"
+      );
+
+    const user = await UserModel.findById(ctx.user._id);
+    if (!user) throw new ApolloError("User deleted!", "USER_DELETED");
+
+    return user;
+  }
+
+  @Authorized()
+  @Mutation(() => User)
+  async updateName(@Arg("newName") newName: string, @Ctx() ctx: IGraphContext) {
+    if (!ctx.user?._id)
+      throw new ApolloError(
+        "User not authenticated!",
+        "USER_NOT_AUTHENTICATED"
+      );
+
+    const user = await UserModel.findById(ctx.user._id);
+    if (!user) throw new ApolloError("User deleted!", "USER_DELETED");
+
+    if (!newName) throw new ApolloError("Name can't be empty!");
+    user.name = newName;
+    return await user.save();
+  }
+}
+
+export { User, UserResolver };
