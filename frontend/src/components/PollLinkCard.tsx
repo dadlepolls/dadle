afbd702665435a38d40aa95d5ce3de1a75f7efcb
Peter Kappelt <kappelt.peter@gmail.com>
2021-12-13 22:19:52 +0100

    Refactoring: move poll link card with updatedAt to own component

---
+++ frontend/src/components/PollLinkCard.tsx
@@ -0,0 +1,40 @@
+import { GetPollsOverview_getPolls } from "@operations/queries/__generated__/GetPollsOverview";
+import { getUserDisplayname } from "@util/getUserDisplayname";
+import { Card, Tooltip, Typography } from "antd";
+import moment from "moment";
+import Link from "next/link";
+import { NextRouter } from "next/router";
+
+const PollLinkCard = ({
+  router,
+  poll,
+}: {
+  router: NextRouter;
+  poll: GetPollsOverview_getPolls;
+}): JSX.Element => {
+  return (
+    <Card
+      style={{ marginTop: "16px", cursor: "pointer" }}
+      size="small"
+      onClick={() => router.push(`/p/${poll.link}`)}
+      hoverable
+    >
+      <Typography.Text strong={true}>
+        <Link href={`/p/${poll.link}`}>{poll.title}</Link>
+      </Typography.Text>
+      {poll.author.user || poll.author.anonName ? (
+        <small>&nbsp; von {getUserDisplayname(poll.author)}</small>
+      ) : null}
+      {poll.updatedAt ? (
+        <Tooltip title={moment(poll.updatedAt).format("DD.MM.YY HH:MM")}>
+          <Typography.Text style={{ float: "right" }}>
+            {moment(poll.updatedAt).fromNow()}
+          </Typography.Text>
+        </Tooltip>
+      ) : null}
+    </Card>
+  );
+};
+
+export { PollLinkCard };
+
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,14 +3,12 @@ import { useQuery } from "@apollo/client";
 import { ErrorPage } from "@components/ErrorPage";
 import { LoadingCard } from "@components/LoadingCard";
 import { PollEditDialog } from "@components/PollEditDialog";
+import { PollLinkCard } from "@components/PollLinkCard";
 import { GET_POLLS_OVERVIEW } from "@operations/queries/GetPollsOverview";
 import { GetPollsOverview } from "@operations/queries/__generated__/GetPollsOverview";
-import { getUserDisplayname } from "@util/getUserDisplayname";
-import { Card, Tooltip, Typography } from "antd";
 import moment from "moment";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -40,28 +38,7 @@ const Home: NextPage = () => {
       {polls
         .sort((b, a) => moment(a.updatedAt).diff(moment(b.updatedAt)))
         .map((poll, idx) => (
-          <Card
-            key={idx}
-            style={{ marginTop: "16px", cursor: "pointer" }}
-            size="small"
-            onClick={() => router.push(`/p/${poll.link}`)}
-          >
-            <Typography.Text strong={true}>
-              <Link key={idx} href={`/p/${poll.link}`}>
-                {poll.title}
-              </Link>
-            </Typography.Text>
-            {poll.author.user || poll.author.anonName ? (
-              <small>&nbsp; von {getUserDisplayname(poll.author)}</small>
-            ) : null}
-            {poll.updatedAt ? (
-              <Tooltip title={moment(poll.updatedAt).format("DD.MM.YY HH:MM")}>
-                <Typography.Text style={{ float: "right" }}>
-                  {moment(poll.updatedAt).fromNow()}
-                </Typography.Text>
-              </Tooltip>
-            ) : null}
-          </Card>
+          <PollLinkCard key={idx} poll={poll} router={router} />
         ))}
     </>
   );
