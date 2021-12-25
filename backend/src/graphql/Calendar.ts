b53dc4c7b4dac88e4f0f33e3c0da38c63b2314eb
Peter Kappelt <kappelt.peter@gmail.com>
2021-12-25 23:59:09 +0100

    mutations to enabled, disable and delete calendars

--- backend/src/graphql/Calendar.ts
+++ backend/src/graphql/Calendar.ts
@@ -60,6 +60,62 @@ class CalendarResolver {
 
     return user.calendars;
   }
+
+  @Authorized()
+  @Mutation(() => Calendar)
+  async setCalendarEnabled(
+    @Arg("calendarId", () => ID) calendarId: string,
+    @Arg("enabled") enabled: boolean,
+    @Ctx() ctx: IGraphContext
+  ) {
+    if (!ctx.user?._id)
+      throw new ApolloError(
+        "User not authenticated!",
+        "USER_NOT_AUTHENTICATED"
+      );
+
+    const user = await UserModel.findById(ctx.user._id);
+    if (!user) throw new ApolloError("User deleted!", "USER_DELETED");
+
+    const calendar = user.calendars?.find((c) => c._id == calendarId);
+    if (!calendar)
+      throw new ApolloError("Calendar not found!", "CALENDAR_NOT_FOUND");
+
+    calendar.enabled = enabled;
+    user.markModified("calendars");
+    await user.save();
+
+    return user.calendars?.find((c) => c._id == calendarId);
+  }
+
+  @Mutation(() => Boolean, { nullable: true })
+  async deleteCalendar(
+    @Arg("calendarId", () => ID) calendarId: string,
+    @Ctx() ctx: IGraphContext
+  ) {
+    if (!ctx.user?._id)
+      throw new ApolloError(
+        "User not authenticated!",
+        "USER_NOT_AUTHENTICATED"
+      );
+
+    const user = await UserModel.findById(ctx.user._id);
+    if (!user) throw new ApolloError("User deleted!", "USER_DELETED");
+
+    const calendar = user.calendars?.find((c) => c._id == calendarId);
+    if (!calendar)
+      throw new ApolloError("Calendar not found!", "CALENDAR_NOT_FOUND");
+
+    await UserModel.findByIdAndUpdate(
+      ctx.user._id,
+      {
+        $pull: {
+          calendars: { _id: calendarId },
+        },
+      },
+      { new: true }
+    );
+  }
 }
 
 export { Calendar, CalendarResolver };
