3fc3bf46e790b76b850cd98333c927cc7f4ddd5f
Peter Kappelt <kappelt.peter@gmail.com>
2021-12-26 00:54:53 +0100

    proactively detect expired tokens

--- frontend/src/components/AuthContext.tsx
+++ frontend/src/components/AuthContext.tsx
@@ -2,8 +2,30 @@ import { useQuery } from "@apollo/client";
 import { GET_ME } from "@operations/queries/GetMe";
 import { GetMe, GetMe_me } from "@operations/queries/__generated__/GetMe";
 import { message } from "antd";
+import jwt_decode from "jwt-decode";
 import * as ls from "local-storage";
-import React, { useContext, useState } from "react";
+import { Router } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
+
+const tokenExpiredObservers: (() => unknown)[] = [];
+
+Router.events.on("routeChangeComplete", () => {
+  //validate the JWT token on each route change
+  if (typeof localStorage === "undefined") return;
+
+  const storedToken = localStorage.getItem("token");
+  if (!storedToken) return;
+
+  try {
+    const token = jwt_decode<{ exp?: number }>(storedToken);
+    if (token.exp && token.exp * 1000 < new Date().getTime() + 60 * 1000)
+      throw new Error(); //token will expiry in less than a minute
+  } catch (_) {
+    message.warning("Du wurdest abgemeldet, weil die Sitzung abgelaufen ist.");
+    localStorage.removeItem("token");
+    tokenExpiredObservers.forEach((o) => o());
+  }
+});
 
 const AuthContext = React.createContext<{
   user?: GetMe_me;
@@ -30,8 +52,16 @@ const AuthContextProvider = ({
       : undefined;
 
   const [_, setDummyState] = useState(0); //dummy state to force update when trying login
-
+  const forceReRender = () => setDummyState((x) => x + 1);
   const [authLoading, setAuthLoading] = useState(true);
+
+  useEffect(() => {
+    tokenExpiredObservers.push(forceReRender);
+    return () => {
+      const idx = tokenExpiredObservers.indexOf(forceReRender);
+      if (idx) tokenExpiredObservers.splice(idx, 1);
+    };
+  }, [forceReRender]);
 
   const { client, data } = useQuery<GetMe>(GET_ME, {
     skip: !token,
@@ -43,7 +73,7 @@ const AuthContextProvider = ({
     onError: (error) => {
       message.error("Anmelden fehlgeschlagen: " + error.message);
       localStorage.removeItem("token");
-      setDummyState((x) => x + 1);
+      forceReRender();
       setAuthLoading(false);
     },
   });
@@ -53,11 +83,11 @@ const AuthContextProvider = ({
       value={{
         user: data?.me,
         authLoading,
-        tryLogin: () => setDummyState((x) => x + 1),
+        tryLogin: forceReRender,
         logout: () => {
           localStorage.removeItem("token");
           client.resetStore();
-          setDummyState((x) => x + 1);
+          forceReRender();
           message.success("Erfolgreich abgemeldet!");
         },
         isAuthenticated: data?.me && data.me._id ? true : false,
