741406723e25c0719e48e2c89e16a144d3f50636
Peter Kappelt <kappelt.peter@gmail.com>
2021-12-30 08:24:10 +0100

    express user definition required here also

--- backend/test/util/printGraphSchema.ts
+++ backend/test/util/printGraphSchema.ts
@@ -2,6 +2,15 @@ import "reflect-metadata";
 import { printSchema } from "graphql";
 import { stdout } from "process";
 import { buildAppSchema } from "../../src/graphql/BuildSchema";
+import { IUser } from "../../src/util/types";
+
+/* eslint-disable */
+declare global {
+  namespace Express {
+    interface User extends Partial<IUser> {}
+  }
+}
+/* eslint-enable */
 
 const main = async () => {
   stdout.write(printSchema(await buildAppSchema()));
