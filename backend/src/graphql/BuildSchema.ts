20e504b349673e16af90438de282f48199b6483e
Peter Kappelt <kappelt.peter@gmail.com>
2022-01-10 12:20:22 +0100

    refactored code to separate resolvers in matching files

--- backend/src/graphql/BuildSchema.ts
+++ backend/src/graphql/BuildSchema.ts
@@ -5,11 +5,15 @@ import { UserResolver } from "./User";
 import { UserOrAnonResolver } from "./UserOrAnon";
 import { CalendarResolver } from "./Calendar";
 import { PollAvailabilityHintResolver } from "./AvailabilityHint";
+import { PollParticipationResolver } from "./PollParticipation";
+import { PollCommentResolver } from "./PollComment";
 
 const buildAppSchema = async () =>
   await buildSchema({
     resolvers: [
       PollResolver,
+      PollParticipationResolver,
+      PollCommentResolver,
       UserResolver,
       UserOrAnonResolver,
       CalendarResolver,
--- backend/src/graphql/PollParticipation.ts
+++ backend/src/graphql/PollParticipation.ts
@@ -1,7 +1,19 @@
-import { Field, ID, InputType, ObjectType } from "type-graphql";
-import { IPollChoice, IPollParticipation } from "../util/types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  ID,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
+import { IGraphContext, IPollChoice, IPollParticipation } from "../util/types";
+import { Poll } from "./Poll";
 import { PollChoice } from "./PollChoice";
 import { UserOrAnon } from "./UserOrAnon";
+import { Poll as PollModel } from "../db/models";
+import { ApolloError } from "apollo-server-express";
 
 @ObjectType()
 class PollParticipation implements IPollParticipation {
@@ -28,7 +40,8 @@ class PollParticipationInput implements Partial<IPollParticipation> {
 
   @Field({
     nullable: true,
-    description: "Name of participation author, in case user is not authenticated",
+    description:
+      "Name of participation author, in case user is not authenticated",
   })
   anonName?: string;
 
@@ -41,4 +54,132 @@ class PollParticipationInput implements Partial<IPollParticipation> {
   }
 }
 
-export { PollParticipation, PollParticipationInput };
+@Resolver(PollParticipation)
+class PollParticipationResolver {
+  @Mutation(() => Poll)
+  async createOrUpdateParticipation(
+    @Arg("pollId", () => ID) pollId: string,
+    @Arg("participation") participationInput: PollParticipationInput,
+    @Ctx() ctx: IGraphContext
+  ) {
+    const dbPoll = await PollModel.findOne({ _id: pollId }).exec();
+    if (!dbPoll) {
+      throw new ApolloError("Couldn't find poll!", "POLL_NOT_FOUND");
+    }
+
+    if (!participationInput.anonName && !ctx.user?._id)
+      throw new ApolloError(
+        "anonName must be given in case user is not authenticated",
+        "INVALID_REQUEST"
+      );
+
+    //validate that all options exist for this poll
+    for (const opt of participationInput.choices) {
+      if (!dbPoll.options?.some((o) => o._id == opt.option.toString())) {
+        throw new ApolloError(
+          "Response for unkown option given",
+          "REPONSE_TO_INVALID_OPTION",
+          { invalidOptionId: opt.option.toString() }
+        );
+      }
+    }
+
+    if (participationInput._id) {
+      //participation does already exist, try to update
+      const participation = dbPoll.participations?.find(
+        (p) => p._id == participationInput._id
+      );
+
+      if (!participation)
+        throw new ApolloError(
+          "Couldn't find participation!",
+          "PARTICIPATION_NOT_FOUND"
+        );
+
+      if (
+        participation.author.userId &&
+        participation.author.userId != ctx.user?._id
+      )
+        throw new ApolloError(
+          "Can't edit participation of other users",
+          "INSUFFICIENT_PERMISSIONS"
+        );
+
+      if (participation.author.userId && participationInput.anonName)
+        throw new ApolloError(
+          "Can't change a user-based to anonymous participation",
+          "INSUFFICIENT_PERMISSIONS"
+        );
+
+      participation.author = {
+        anonName: participationInput.anonName,
+        userId: !participationInput.anonName ? ctx.user?._id : undefined,
+      };
+
+      for (const opt of participationInput.choices) {
+        //update existing choices
+        participation.choices.forEach((c) => {
+          if (c.option == opt.option) c.choice = opt.choice;
+        });
+        //create new choices
+        if (!participation.choices.some((c) => c.option == opt.option)) {
+          participation.choices.push(opt);
+        }
+      }
+    } else {
+      if (!dbPoll.participations) dbPoll.participations = [];
+      dbPoll.participations.push({
+        ...participationInput,
+        author: {
+          anonName: participationInput.anonName,
+          userId: !participationInput.anonName ? ctx.user?._id : undefined,
+        },
+      });
+    }
+
+    await dbPoll.save();
+    return dbPoll.toObject();
+  }
+
+  @Mutation(() => Poll)
+  async deleteParticipation(
+    @Arg("pollId", () => ID) pollId: string,
+    @Arg("participationId", () => ID) participationId: string,
+    @Ctx() ctx: IGraphContext
+  ) {
+    const dbPoll = await PollModel.findOne({ _id: pollId }).exec();
+    if (!dbPoll) throw new ApolloError("Couldn't find poll", "POLL_NOT_FOUND");
+
+    const participation = dbPoll.participations?.find(
+      (p) => p._id == participationId
+    );
+    if (!participation)
+      throw new ApolloError(
+        "Couldn't find participation",
+        "PARTICIPATION_NOT_FOUND"
+      );
+
+    if (
+      participation.author.userId &&
+      participation.author.userId != ctx.user?._id
+    )
+      throw new ApolloError(
+        "Can't delete participations of other users!",
+        "INSUFFICIENT_PERMISSIONS"
+      );
+
+    return (
+      await PollModel.findByIdAndUpdate(
+        pollId,
+        {
+          $pull: {
+            participations: { _id: participationId },
+          },
+        },
+        { new: true }
+      )
+    )?.toObject();
+  }
+}
+
+export { PollParticipation, PollParticipationInput, PollParticipationResolver };
