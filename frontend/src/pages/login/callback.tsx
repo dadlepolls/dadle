0ea131cd3b5d5faafdae5a12b469c81816aa929d
Peter Kappelt <kappelt.peter@gmail.com>
2022-01-20 16:41:35 +0100

    show optional authentication error message

--- frontend/src/pages/login/callback.tsx
+++ frontend/src/pages/login/callback.tsx
@@ -9,10 +9,16 @@ const LoginCallbackPage: NextPage = () => {
   const router = useRouter();
   const { tryLogin } = useAuth();
   const [isMissingTokenInQuery, setIsMissingTokenInQuery] = useState(false);
+  const [failureMessage, setFailureMessage] = useState<string | undefined>();
 
   useEffect(() => {
     if (router.isReady && !router.query.token) setIsMissingTokenInQuery(true);
   }, [router.isReady, router.query.token]);
+
+  useEffect(() => {
+    if (router.isReady && router.query.failureMsg)
+      setFailureMessage(String(router.query.failureMsg));
+  }, [router.isReady, router.query.failureMsg]);
 
   useEffect(() => {
     if (typeof localStorage !== undefined && router.query.token) {
@@ -23,8 +29,10 @@ const LoginCallbackPage: NextPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router.isReady, router.query.token]);
 
-  if (isMissingTokenInQuery) {
-    return <ErrorPage error={"Es wurde kein Token erzeugt"} />;
+  if (isMissingTokenInQuery || failureMessage) {
+    if (failureMessage)
+      return <ErrorPage error={`Fehlercode: ${failureMessage}`} />;
+    else return <ErrorPage error={"Es wurde kein Token erzeugt"} />;
   }
 
   return <LoadingCard title="Anmeldung läuft..." />;
