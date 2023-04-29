bdf8fd7c46869997be9781fecbfcc9b953d12fe5
Peter Kappelt <kappelt.peter@gmail.com>
2023-04-29 19:00:35 +0200

    Fix missing i18n on poll page

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -1,8 +1,11 @@
+const path = require("path");
+
 /** @type {import("next-i18next").UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de"],
+    localePath: path.resolve('./public/locales'),
     returnNull: false,  //this is a workaround, remove when https://github.com/i18next/i18next/issues/1884 is closed
   },
 };
